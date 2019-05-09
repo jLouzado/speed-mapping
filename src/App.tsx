68299ef96b863a24ad486bc7d780f0732b7c1ce1
Joel <jlouzado@gmail.com>
2019-05-09 17:25:30 +0530

    add grid,  move links below nodes and apply vertical drift once per connection

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ type Circle = {name: string; fx?: number; fy?: number; maturity?: number}
 type Link = {source: string; target: string}
 
 class App extends PureComponent<AppProps> {
-  svgEl: HTMLDivElement | null
+  svgEl: SVGSVGElement | null
   constructor(props: AppProps) {
     super(props)
     this.svgEl = null
@@ -24,8 +24,7 @@ class App extends PureComponent<AppProps> {
       .forceSimulation()
       .nodes(NodePaths.nodes)
       .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('charge_force', d3.forceManyBody().strength(-90))
-      .force('gravity', d3.forceY(50))
+      .force('charge_force', d3.forceManyBody().strength(-10))
 
     const link_force = d3.forceLink(NodePaths.links).id((d: any) => d.name)
 
@@ -33,9 +32,18 @@ class App extends PureComponent<AppProps> {
 
     const svg = d3
       .select(this.svgEl)
-      .append('svg')
       .attr('width', width)
       .attr('height', height)
+
+    const link = svg
+      .append('g')
+      .attr('class', 'links')
+      .selectAll('line')
+      .data<Link>(NodePaths.links)
+      .enter()
+      .append('line')
+      .style('stroke', '#999')
+      .style('stroke-width', 2)
 
     const node = svg
       .append('g')
@@ -58,20 +66,11 @@ class App extends PureComponent<AppProps> {
       .attr('x', 6)
       .attr('y', 3)
 
-    const link = svg
-      .append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data<Link>(NodePaths.links)
-      .enter()
-      .append('line')
-      .style('stroke', '#999')
-      .style('stroke-width', 2)
-
     simulation.on('tick', () => {
       const alpha = simulation.alpha()
       const k = 50 * alpha
       node.attr('transform', (d: any) => 'translate(' + d.x + ',' + d.y + ')')
+      // Drift each node horizontally to it's maturity
       node.each((d: any) => {
         if (d.maturity) {
           const next = d.x + (width * (d.maturity / 100) - d.x) * alpha
@@ -79,10 +78,25 @@ class App extends PureComponent<AppProps> {
         }
       })
 
+      const sources: {
+        [key: string]: boolean
+      } = {}
+
+      const targets: {
+        [key: string]: boolean
+      } = {}
+
       link
+        // Use each link to drift each node vertically
         .each((d: any) => {
-          d.source.y -= k / 2
-          d.target.y += k
+          if (!sources[d.source.name]) {
+            d.source.y -= k
+            sources[d.source.name] = true
+          }
+          if (!targets[d.target.name]) {
+            d.target.y += k * 3
+            targets[d.target.name] = true
+          }
         })
         .attr('x1', (d: any) => d.source.x)
         .attr('y1', (d: any) => d.source.y)
@@ -93,11 +107,91 @@ class App extends PureComponent<AppProps> {
 
   render() {
     const {height, width} = this.props
+    const mapHeight = height - 20
+    const mapWidth = width - 20
+    var custMark = mapWidth / 4
+    var prodMark = mapWidth / 2
+    var commMark = (mapWidth / 4) * 3
+    var visMark = mapHeight / 2
+
     return (
-      <div
-        style={{width, height, border: '1px solid #bada55'}}
-        ref={el => (this.svgEl = el)}
-      />
+      <div style={{width, height, border: '1px solid #bada55'}}>
+        <svg ref={el => (this.svgEl = el)}>
+          <g id="grid">
+            <g
+              id="value chain"
+              transform={`translate(0,${mapHeight}) rotate(270)`}
+            >
+              <line x1="0" y1="0" x2={mapHeight} y2="0" stroke="black" />
+              <line
+                x1="-2em"
+                y1={custMark}
+                x2={mapHeight}
+                y2={custMark}
+                stroke="black"
+                strokeDasharray="5,5"
+              />
+              <line
+                x1="-2em"
+                y1={prodMark}
+                x2={mapHeight}
+                y2={prodMark}
+                stroke="black"
+                strokeDasharray="5,5"
+              />
+              <line
+                x1="-2em"
+                y1={commMark}
+                x2={mapHeight}
+                y2={commMark}
+                stroke="black"
+                strokeDasharray="5,5"
+              />
+              <text x="0" y="-0.2em" textAnchor="start">
+                Invisible
+              </text>
+              <text
+                x={visMark}
+                y="-0.2em"
+                textAnchor="middle"
+                fontWeight="bold"
+              >
+                Value Chain
+              </text>
+              <text x={mapHeight} y="-0.2em" textAnchor="end">
+                Visible
+              </text>
+            </g>
+            <g id="Evolution" transform={`translate(0,${mapHeight})`}>
+              <line x1="0" y1="0" x2={mapHeight} y2="0" stroke="black" />
+              <text x="0" y="1em" textAnchor="start">
+                Genesis
+              </text>
+              <text x={custMark} y="1em" textAnchor="start">
+                &nbsp;Custom
+              </text>
+              <text x={custMark} y="2em" textAnchor="start">
+                &nbsp;built
+              </text>
+              <text x={prodMark} y="1em" textAnchor="start">
+                &nbsp;Product
+              </text>
+              <text x={prodMark} y="2em" textAnchor="start">
+                &nbsp;(+ rental)
+              </text>
+              <text x={commMark} y="1em" textAnchor="start">
+                &nbsp;Commodity
+              </text>
+              <text x={commMark} y="2em" textAnchor="start">
+                &nbsp;(+ utility)
+              </text>
+              <text x={mapHeight} y="1.5em" textAnchor="end" fontWeight="bold">
+                Evolution
+              </text>
+            </g>
+          </g>
+        </svg>
+      </div>
     )
   }
 }
